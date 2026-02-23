76a0cb5f2d21e8c7df58eb23fd0b6a20b2b7dcea
Raphael102030 <raphaelsami06@gmail.com>
2026-02-23 17:59:16 +0100

    Ceci est un message de salutation pour tout le backend ici present

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+$message = "Salut tout le monde , ici on se donne à fonds pour ce projet 
+Alors , je nous souhaite un bon travail , et merci pour votre confiance et contribution dans tout ça !";
+
+echo $message;
